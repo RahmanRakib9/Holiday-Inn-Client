d8c7f0eb764bb9e82d1c876ad627962da2c37859
Rahman Rakib <rahmanrakibur6589@gmail.com>
2022-04-16 00:02:27 +0600

    header updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -10,19 +10,19 @@ const Header = () => {
           <>
                <Navbar bg="light" variant="light" sticky="top" collapseOnSelect expand="lg" >
                     <Container>
-                         <Navbar.Brand href="/home#home">Holiday Inn</Navbar.Brand>
+                         <Navbar.Brand href="/" style={{ fontSize: 'x-large', color: "black", fontFamily: 'fantasy' }}>Holiday Inn</Navbar.Brand>
                          <Navbar.Toggle />
                          <Navbar.Collapse className="justify-content-end">
-                              <Nav.Link href='/home#aboutHotel'>About</Nav.Link>
-                              <Nav.Link href='/home#rooms'>Rooms</Nav.Link>
-                              <Nav.Link to="">Services</Nav.Link>
+                              <Link to='/' className='link-style'>About</Link>
+                              <Link to='/' className='link-style'>Rooms</Link>
+                              <Link to="/" className='link-style'>Services</Link>
 
-                              <Nav.Link href='/login'>{user.email ?
+                              <Link to='/login'>{user.email ?
                                    <button className='authentication-btn' onClick={signOutGoogle}>Log out</button> :
                                    <button className='authentication-btn' onClick={signInWithGoogle}>Login</button>
-                              }</Nav.Link>
+                              }</Link>
 
-                              <Nav.Link to="/register"><button className='authentication-btn'>Sign up</button></Nav.Link>
+                              <Link to="/register"><button className='authentication-btn'>Sign up</button></Link>
                          </Navbar.Collapse>
                     </Container>
                </Navbar>
