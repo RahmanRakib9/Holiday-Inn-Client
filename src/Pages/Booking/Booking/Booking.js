7a6dcc26040e75cb54cd76754333af06515b9228
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2022-08-29 22:33:07 +0600

    add some comment on Booking page

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,70 +1,101 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row, } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
-import './Booking.css'
-import Complimentary from './Complimentary';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faPerson } from '@fortawesome/free-solid-svg-icons'
-import BookingTable from './BookingTable';
-import Footer from '../../Shared/Footer/Footer';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import "./Booking.css";
+import Complimentary from "./Complimentary";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBed, faPerson } from "@fortawesome/free-solid-svg-icons";
+import BookingTable from "./BookingTable";
+import Footer from "../../Shared/Footer/Footer";
 
 const Booking = () => {
-     const { id } = useParams();
-     const [booking, setBooking] = useState({});
+  const { id } = useParams();
+  const [booking, setBooking] = useState({});
 
-     useEffect(() => {
-          fetch(`http://localhost:5000/rooms/${id}`)
-               .then(res => res.json())
-               .then(data => {
-                    setBooking(data)
-                    console.log('dataaaaaa',data);
-               })
-     }, [id]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/rooms/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBooking(data);
+        console.log("dataaaaaa", data);
+      });
+  }, [id]);
 
-
-     return (
-          <section >
-               <header className='booking-header mt-2'>
-                    <div className='container alignment'>
-
-                         <div>
-                              <h1 >{booking.roomTitle}</h1>
-                              <Link to='/'>Home</Link> / <span>Room-Details</span>
-                         </div>
-
-                    </div>
-               </header>
-               <Container className='d-flex justify-content-center'>
-                    <Row className='w-100'>
-                         <Col xs={12} md={7} >
-                              <img style={{ width: "100%", marginTop: "70px" }} src={`data:image/jpeg;base64,${booking.photo}`} alt="" />
-                              <div className='room-element-container'>
-                                   <span className='room-element'>Price in BDT: {booking.price}৳</span>
-                                   <span className='room-element'><FontAwesomeIcon icon={faBed} /> Bed-Type: {booking.bedType}</span>
-                                   <span className='room-element'><FontAwesomeIcon icon={faPerson} /> Bed-Capacity: {booking.bedCapacity}</span>
-                              </div>
-                              <div style={{ fontSize: 'medium', fontFamily: "font-family:Arial, Helvetica, sans-serif" }}>
-                                   <p>{booking.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio tenetur, non consequuntur provident sapiente et ullam natus quos perferendis. </p>
-                              </div>
-                         </Col>
-                         <Col xs={12} md={5} className='d-flex justify-content-center align-items-center'>
-                              <Complimentary />
-                         </Col>
-                    </Row>
-
-               </Container>
-               <section style={{ marginTop: "30px" }}>
-                    <Container>
-                         <BookingTable booking={booking} ></BookingTable>
-                    </Container>
-               </section>
-               <section style={{ marginTop: "80px" }}>
-                    <hr />
-                    <Footer></Footer>
-               </section>
-          </section>
-     );
+  return (
+    <section>
+      {/* section header start from here */}
+      <header className="booking-header mt-2">
+        <div className="container alignment">
+          <div className="mt-4">
+            <h1>{booking.roomTitle}</h1>
+            <Link className="homePage-option" to="/">Home</Link> / <span className="active-page">Room-Details</span>
+          </div>
+        </div>
+      </header>
+      {/* section header end here */}
+      <Container className="d-flex justify-content-center">
+        {/* Row start from here */}
+        <Row className="w-100">
+          {/* col-1 start from here */}
+          <Col xs={12} md={7}>
+            <img
+              style={{ width: "100%", marginTop: "70px" }}
+              src={`data:image/jpeg;base64,${booking.photo}`}
+              alt=""
+            />
+            <div className="room-element-container">
+              <span className="room-element">
+                Price in BDT: {booking.price}৳
+              </span>
+              <span className="room-element">
+                <FontAwesomeIcon icon={faBed} /> Bed-Type: {booking.bedType}
+              </span>
+              <span className="room-element">
+                <FontAwesomeIcon icon={faPerson} /> Bed-Capacity:{" "}
+                {booking.bedCapacity}
+              </span>
+            </div>
+            <div
+              style={{
+                fontSize: "medium",
+                fontFamily: "font-family:Arial, Helvetica, sans-serif",
+              }}
+            >
+              <p>
+                {booking.description} Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Voluptates distinctio tenetur, non
+                consequuntur provident sapiente et ullam natus quos perferendis.{" "}
+              </p>
+            </div>
+          </Col>
+          {/* col-1 end here */}
+          {/* col-2 start from here */}
+          <Col
+            xs={12}
+            md={5}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <Complimentary />
+          </Col>
+          {/* col-2 end here */}
+        </Row>
+        {/* Row end here */}
+      </Container>
+      {/* Booking table section start here */}
+      <section style={{ marginTop: "30px" }}>
+        <Container>
+          <BookingTable booking={booking}></BookingTable>
+        </Container>
+      </section>
+      {/* Booking table section end here */}
+      <section style={{ marginTop: "80px" }}>
+        <hr />
+        {/* footer start from here */}
+        <Footer></Footer>
+        {/* footer end here */}
+      </section>
+    </section>
+  );
 };
 
 export default Booking;
-
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link, NavLink } from 'react-router-dom';
-import useAuth from '../../../hooks/useAuth';
-import './Header.css'
-import { HashLink } from 'react-router-hash-link';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import "./Header.css";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
-     const { user, signInWithGoogle, signOutGoogle } = useAuth();
-     return (
-          <>
-               <Container>
-                    <Navbar variant="light" collapseOnSelect expand="lg" className='w-100 d-flex justify-content-center'>
+  const { user, signInWithGoogle, signOutGoogle } = useAuth();
+  return (
+    <>
+      <Container>
+        <Navbar
+          variant="light"
+          collapseOnSelect
+          expand="lg"
+          className="w-100 d-flex justify-content-center"
+        >
+          <Navbar.Brand
+            href="/home#banner"
+            style={{
+              fontSize: "x-large",
+              color: "black",
+              fontFamily: "fantasy",
+            }}
+          >
+            Holiday Inn
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <HashLink to="/home#about" className="link-style">
+              About
+            </HashLink>
+            <HashLink to="/home#rooms" className="link-style">
+              Rooms
+            </HashLink>
+            <HashLink to="/home#services" className="link-style">
+              Services
+            </HashLink>
+            <HashLink to="/home#contact" className="link-style">
+              Contact
+            </HashLink>
 
-                         <Navbar.Brand href="/home#banner" style={{ fontSize: 'x-large', color: "black", fontFamily: 'fantasy' }}>Holiday Inn</Navbar.Brand>
-                         <Navbar.Toggle />
-                         <Navbar.Collapse className="justify-content-end">
-                              <HashLink to='/home#about' className='link-style'>About</HashLink>
-                              <HashLink to='/home#rooms' className='link-style'>Rooms</HashLink>
-                              <HashLink to="/home#services" className='link-style'>Services</HashLink>
-                              <HashLink to="/home#contact" className='link-style'>Contact</HashLink>
+            {user.email ? (
+              <button className="authentication-btn" onClick={signOutGoogle}>
+                Log Out
+              </button>
+            ) : (
+              <Link to="/login">
+                <button className="authentication-btn">Login</button>
+              </Link>
+            )}
 
-                              {user.email ?
-                                   <button className='authentication-btn' onClick={signOutGoogle} >Log Out</button> :
-                                   <Link to="/login"><button className='authentication-btn'>Login</button></Link>
-                              }
-
-
-                              <Link to="/register"><button className='authentication-btn'>Sign up</button></Link>
-                         </Navbar.Collapse>
-
-                    </Navbar>
-               </Container>
-          </>
-     );
+            <Link to="/register">
+              <button className="authentication-btn">Sign up</button>
+            </Link>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </>
+  );
 };
 
 export default Header;
